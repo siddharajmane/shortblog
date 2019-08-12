7c5e9ce6723e4aa8346ea4851d80444b1d94ab35
siddharaj <manesiddharaj8@gmail.com>
2019-08-13 01:14:53 +0530

    commit

--- app.js
+++ app.js
@@ -186,7 +186,11 @@ app.get("/sample",function(req,res){
   res.render("sample");
 });
 
+let port=process.env.PORT;
+if(port==null || port==""){
+  port=3000;
+}
 
-app.listen(4000,function(){
-  console.log("server started at port 4000");
+app.listen(port,function(){
+  console.log("server");
 });
